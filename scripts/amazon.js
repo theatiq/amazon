fd4d069780b248e3de46c62338dd62f64a6a3cd5
theatiq <theatiq@gmail.com>
2024-10-01 16:18:48 +0600

    All products added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,55 +1,29 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "lack and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    },
-]
+
+
+let productsHTML = ''
 
 products.forEach((product) => {
-    const html = `
+  productsHTML += `
             <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/athletic-cotton-socks-6-pairs.jpg">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-45.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              87
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $10.90
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -79,5 +53,8 @@ products.forEach((product) => {
           </button>
         </div>
     `
-    console.log(html);
 })
+
+console.log(productsHTML);
+
+document.querySelector(".js-products-grid").innerHTML = productsHTML
